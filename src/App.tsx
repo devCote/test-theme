033f771388a6287c59a1ddce90a7bf4837e0aab9
devCote <bahanenko@gmail.com>
2023-08-10 00:55:38 +0300

    app classnames and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@ import "./App.scss";
 import MatrixRainEffect from "./MatrixRain";
 
 function App() {
-
-
   return (
-    <div className="mainContainer">
-      <MatrixRainEffect />
+    <div className='app-container'>
+      <div className="matrix-container">
+        <MatrixRainEffect />
+      </div>
+      <div className="content-container">
+        <h1>Hello There</h1>
+        <p>
+          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+          exercitation amet. Nisi anim cupidatat excepteur officia.
+          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+          voluptate dolor minim nulla est proident. Nostrud officia pariatur ut
+          officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
+          commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
+          Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
+          officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
+          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
+          consectetur et est culpa et culpa duis.
+        </p>
+      </div>
     </div>
   );
 }
